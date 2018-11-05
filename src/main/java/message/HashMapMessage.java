20ec65ac9a8683c8c5ac8ab9019948a44322b6ac
BUAA_Blockchain <BUAA_Blockchain@blockchain>
2018-11-05 19:30:14 +0800

    fix

---
+++ src/main/java/message/HashMapMessage.java
@@ -0,0 +1,14 @@
+package message;
+
+import java.util.HashSet;
+
+import bean.Transaction;
+
+public class HashMapMessage {
+
+	public HashSet<Transaction> transactionSet ;
+	
+	public int nodeLabel;
+	
+	public int batchNum; 
+}
